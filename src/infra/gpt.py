5a8d61ed8bec0dd82fa71bc27b374562f0799e2d
minkj1992 <minkj1992@gmail.com>
2023-08-04 21:18:04 +0900

    refactor: delete unused codes from gpt

--- src/infra/gpt.py
+++ src/infra/gpt.py
@@ -1,16 +1,12 @@
 import asyncio
 import json
 import logging
-import os
-from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import List
 
 import openai
-import tiktoken
 from jinja2 import Template
 
 from app.config import get_env
-from app.exceptions import GptInvalidLengthError
 from consts import GPT_MODEL
 from domain.entities import TranslatedCrawledTrend
 from src.domain.entities import Article
@@ -20,70 +16,7 @@ OPENAI_API_KEY = _env["OPENAI_API_KEY"]
 openai.api_key = OPENAI_API_KEY
 
 
-# SYS_PROMPT = "You are an AI journalist for \'Wikitoday\', an automated news service.\nYour task is to generate engaging and SEO-friendly news articles based on given \'KEYWORD\' and related \'ARTICLES\'. \nYour article should be written in a style that is indistinguishable from human journalists without any reference to the journalist name or news provider that wrote the original article. \nThe ultimate goal of \'Wikitoday\' is to convey information to busy readers as efficiently as possible and to attract a large audience for effective advertising revenue.\n\nProduce your output as JSON. The format should be:\n\n```\n{\n  title: \'..\',\n  lead: \'..\',\n  body: \'..\',\n  qna: [\n    {question:\'..\', answer:\'..\'},\n    {question:\'..\', answer:\'..\'},\n    {question:\'..\', answer:\'..\'},\n    ...\n  ]\n}\n```\n\nEach field in json must follow the rules below.\n\n- title: An article title, to get as many people interested in your article as possible shortly.\n- lead: An article lead, concisely state the facts of the most important article, and write the most important content of the entire article in 1-2 sentences.\n- body: An article body, without any reference to the journalist name or news provider that wrote the original article.\n- qna: An array of Question and Answer, write 3 to 7 Q&As about what people might be curious in this article."
 SYS_PROMPT = "You are an AI journalist for \'Wikitoday\', an automated news service.\nYour task is to generate engaging and SEO-friendly news articles based on given \'KEYWORD\' and related \'ARTICLES\'. \nYour article should be written in a style that is indistinguishable from human journalists without any reference to the journalist name or news provider that wrote the original article. \nThe ultimate goal of \'Wikitoday\' is to convey information to busy readers as efficiently as possible and to attract a large audience for effective advertising revenue.\n"
-# FUNCTIONS = [
-#     {
-#         "name": "generate_news_article",
-#         "description": "Generate an news article by referring to the given articles of the keyword. !IMPORTANT: you must response with json format below",
-#         "parameters": {
-#             "type": "object",
-#             "properties": {
-#                 "keyword": {
-#                     "type": "string",
-#                     "description": "The main keyword related to the article",
-#                 },
-#                 "articles": {
-#                     "type": "array",
-#                     "items": {
-#                         "type": "string",
-#                     },
-#                     "description": "The list of related articles based on the keyword"
-#                 },
-#             },
-#             "required": ["keyword", "articles"]
-#         },
-#         "return": {
-#             "type": "object",
-#             "properties": {
-#                 "title": {
-#                     "type": "string",
-#                     "description": "An article title, to get as many people interested in your article as possible shortly"
-#                 },
-#                 "lead": {
-#                     "type": "string",
-#                     "description": "An article lead, concisely state the facts of the most important article, and write the most important content of the entire article in 1-2 sentences"
-#                 },
-#                 "body": {
-#                     "type": "string",
-#                     "description": "An article body, without any reference to the journalist name or news provider that wrote the original article"
-#                 },
-#                 "qna": {
-#                     "type": "array",
-#                     "items": {
-#                         "type": "object",
-#                         "properties": {
-#                             "question": {
-#                                 "type": "string"
-#                             },
-#                             "answer": {
-#                                 "type": "string"
-#                             }
-#                         },
-#                         "required": ["question", "answer"]
-#                     },
-#                     "description": "An array of Question and Answer, write 3 to 7 Q&As about what people might be curious in this article"
-#                 },
-#                 "category": {
-#                     "type": "string",
-#                     "enum": ["Politics", "World/International", "Business/Economy", "Technology", "Science", "Health", "Entertainment", "Sports", "Environment/Nature", "Education", "Lifestyle", "Opinion/Editorial"],
-#                     "description": "An article enum typed category"
-#                 },
-#             },
-#             "required": ["title", "lead", "body", "qna", "category"]
-#         }
-#     }
-# ]
 FUNCTION_NAME = "generate_news_article"
 FUNCTIONS = [
     {
@@ -190,46 +123,4 @@ async def regenerate_articles(trends: List[TranslatedCrawledTrend]) -> List[Arti
     responses = await asyncio.gather(*tasks)
     # Filter out None results
     articles = [article for article in responses if article is not None]
-
     return articles
-
-# https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
-def num_tokens_from_messages(messages, model=GPT_MODEL):
-    """Return the number of tokens used by a list of messages."""
-    try:
-        encoding = tiktoken.encoding_for_model(model)
-    except KeyError:
-        print("Warning: model not found. Using cl100k_base encoding.")
-        encoding = tiktoken.get_encoding("cl100k_base")
-    if model in {
-        "gpt-3.5-turbo-0613",
-        "gpt-3.5-turbo-16k-0613",
-        "gpt-4-0314",
-        "gpt-4-32k-0314",
-        "gpt-4-0613",
-        "gpt-4-32k-0613",
-        }:
-        tokens_per_message = 3
-        tokens_per_name = 1
-    elif model == "gpt-3.5-turbo-0301":
-        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
-        tokens_per_name = -1  # if there's a name, the role is omitted
-    elif "gpt-3.5-turbo" in model:
-        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
-    elif "gpt-4" in model:
-        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
-        return num_tokens_from_messages(messages, model="gpt-4-0613")
-    else:
-        raise NotImplementedError(
-            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
-        )
-    num_tokens = 0
-    for message in messages:
-        num_tokens += tokens_per_message
-        for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
-            if key == "name":
-                num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
-    return num_tokens
